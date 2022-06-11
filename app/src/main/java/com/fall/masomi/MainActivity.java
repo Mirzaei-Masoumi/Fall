5c5a4e27dcf07fa3e2dac0d0226e3b56d9ea6caf
parastoo-masoomi <fazy1381@gmail.com>
2022-06-11 17:33:36 +0430

    Fal layout added.

--- app/src/main/java/com/fall/masomi/MainActivity.java
+++ app/src/main/java/com/fall/masomi/MainActivity.java
@@ -2,13 +2,25 @@ package com.fall.masomi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnFall;
+    Button btnPoets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnFall = findViewById(R.id.btnFall);
+        btnPoets = findViewById(R.id.btnPoets);
+        btnFall.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, FalIntroActivity.class);
+            startActivity(intent);
+        });
     }
 }
